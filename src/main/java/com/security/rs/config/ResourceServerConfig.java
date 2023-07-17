4878b340e0a495b6a759ad1d77b6d7d86c2932fd
NAME <mail@gmail.com>
2023-07-17 11:41:52 -0500

    JWT Tokens: Using symmetric keys to validate tokens

--- src/main/java/com/security/rs/config/ResourceServerConfig.java
+++ src/main/java/com/security/rs/config/ResourceServerConfig.java
@@ -1,7 +1,8 @@
 package com.security.rs.config;
 
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,13 +10,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+    //@Autowired
+    //private DataSource dataSource;
+    @Value("${jwt.key}")
+    private String jwtKey;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
@@ -24,7 +29,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Bean
     public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+        //return new JdbcTokenStore(dataSource);
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        var converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtKey);
+        return converter;
     }
 
 }
